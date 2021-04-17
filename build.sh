fa97ff682061f77f3d7f0848ceb931d82c592cd0
antonio-leblanc <antonioleblanc@poli.ufrj.br>
2021-04-17 18:34:32 -0300

    new build

--- build.sh
+++ build.sh
@@ -1,4 +1,15 @@
 echo ''
-echo 'Threejs builder'
+echo ' ==== THREEjs BUILDER ===='
+echo 'npm build'
 npm run build
+
+echo 'git push main'
+git add . 
+git commit -m 'new build'
+git push
+
+echo 'git push gh-pages'
 git subtree push --prefix dist origin gh-pages
+echo ''
+echo ' ==== THREEjs BUILDER ===='
+echo ''
